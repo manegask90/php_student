403de9cd3a8eff61b66429fa2ea57d812d1abda9
Vitalik <vitalikmihaylishin@gmail.com>
2018-06-18 17:10:26 +0300

    delete testing

--- calc/func.php
+++ calc/func.php
@@ -19,9 +19,3 @@ function calculate($a, $b, $op)
             return null;
     }
 }
-
-assert(4 == calculate(2, 2, '+'));
-assert(6 == calculate(2, 3, '*'));
-assert(3 == calculate(5, 2, '-'));
-assert(2 == calculate(6, 3, '/'));
-assert(null == calculate(2, 3, '&&'));
