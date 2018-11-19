4850d919c527dd4bf0f9511495091f343233f7fa
vitalik <vitalik.mihaylishin@gmail.com>
2018-11-19 19:17:52 +0200

    fix master

--- index.php
+++ index.php
@@ -6,37 +6,7 @@
 </head>
 <body>
 
-<!--<ul>-->
-<!--    <li>-->
-<!--        <a href="/?id=1">Cat</a>-->
-<!--    </li>-->
-<!--    <li>-->
-<!--        <a href="/?id=2">Dog</a>-->
-<!--    </li>-->
-<!--    <li>-->
-<!--        <a href="/?id=3">Snake</a>-->
-<!--    </li>-->
-<!--    <li>-->
-<!--        <a href="/?id=4">Bear</a>-->
-<!--    </li>-->
-<!--</ul>-->
-<!---->
-<!--<image src="images/--><?php //echo $images[$img_src]; ?><!--" ></image>-->
 
-<form action="/upload.php" method="post" enctype="multipart/form-data">
-    <input type="file" name="newfile">
-    <input type="submit">
-</form>
-<?php
 
-//$res   = scandir(__DIR__ );
-//var_dump($res);
-//$res[] = 'Custom';
-//
-//$str = implode("\n", $res);
-//
-//$data = file_put_contents(__DIR__ . '/db.txt', $res);
-
-?>
 </body>
 </html>
