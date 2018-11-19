3ba09b52031c109c49b7785976da3b6e2fcc41a3
vitalik <vitalik.mihaylishin@gmail.com>
2018-11-19 17:43:25 +0200

    lessons 4

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-    <title>Массивы</title>
+    <title>PHP</title>
 
     <link href="style.css" rel="stylesheet">
 </head>
@@ -19,21 +19,37 @@ $images = [
 ?>
 
 
-<ul>
-    <li>
-        <a href="/?id=1">Cat</a>
-    </li>
-    <li>
-        <a href="/?id=2">Dog</a>
-    </li>
-    <li>
-        <a href="/?id=3">Snake</a>
-    </li>
-    <li>
-        <a href="/?id=4">Bear</a>
-    </li>
-</ul>
+<!--<ul>-->
+<!--    <li>-->
+<!--        <a href="/?id=1">Cat</a>-->
+<!--    </li>-->
+<!--    <li>-->
+<!--        <a href="/?id=2">Dog</a>-->
+<!--    </li>-->
+<!--    <li>-->
+<!--        <a href="/?id=3">Snake</a>-->
+<!--    </li>-->
+<!--    <li>-->
+<!--        <a href="/?id=4">Bear</a>-->
+<!--    </li>-->
+<!--</ul>-->
+<!---->
+<!--<image src="images/--><?php //echo $images[$img_src]; ?><!--" ></image>-->
 
-<image src="images/<?php echo $images[$img_src]; ?>" ></image>
+<form action="/upload.php" method="post" enctype="multipart/form-data">
+    <input type="file" name="newfile">
+    <input type="submit">
+</form>
+<?php
+
+//$res   = scandir(__DIR__ );
+//var_dump($res);
+//$res[] = 'Custom';
+//
+//$str = implode("\n", $res);
+//
+//$data = file_put_contents(__DIR__ . '/db.txt', $res);
+
+?>
 </body>
 </html>
---
+++ upload.php
@@ -0,0 +1,13 @@
+<?php
+
+var_dump($_FILES);
+
+if (!empty($_FILES)) {
+    $uploaded = $_FILES['newfile'];
+    if ($uploaded['error'] == 0) {
+        move_uploaded_file(
+            $uploaded['tmp_name'],
+            __DIR__ . '/images/test.png'
+        );
+    }
+}
