6b68af7f41bb35c294b1b639a8c8376b740430cb
vitalik <vitalik.mihaylishin@gmail.com>
2018-11-20 11:45:24 +0200

    less 5

--- index.php
+++ index.php
@@ -6,7 +6,32 @@
 </head>
 <body>
 
+<?php
 
+class Table
+{
+    public $color;
+    public $legs;
+
+    public function __construct($color)
+    {
+        $this->color = $color;
+    }
+
+    public function show()
+    {
+        echo 'Table ';
+        echo '<br>';
+        echo $this->color;
+    }
+}
+
+$table1 = new Table('red');
+$table1->show();
+
+
+
+?>
 
 </body>
 </html>
